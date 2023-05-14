53e059bfb89c8b5b67cddf42007daa49fdb83dea
Oleksandra Kryvobok <88559507+OleksandraKryvobok@users.noreply.github.com>
2023-05-14 14:28:51 +0300

    delete contactsApi

--- src/services/contactsApi.js
+++
@@ -1,5 +0,0 @@
-
-
-// fetchContacts - одержання масиву контактів (метод GET) запитом. Базовий тип екшену "contacts/fetchAll".
-// addContact - додавання контакту (метод POST). Базовий тип екшену "contacts/addContact".
-// deleteContact -
